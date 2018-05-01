19af8a6f5fa1a76cb5265040dd64f5ef5ca7fb2d
starship9 <nishanksaini10@gmail.com>
2018-05-01 19:43:42 +0530

    Updated metric curves, dataset

--- metric.R
+++ metric.R
@@ -1,4 +1,4 @@
-algoDF <- read.csv("metrics.csv")
+algoDF <- read.csv("Data/metrics.csv")
 str(algoDF)
 head(algoDF)
 
@@ -6,8 +6,8 @@ library(tidyverse)
 library(RColorBrewer)
 
 ggplot(data = algoDF,mapping = aes(x = X, y = auc)) + geom_point() + geom_line(aes(group = 1)) + ggtitle("AUC(Area under curve) for all algorithms") + xlab("")
-ggplot(data = algoDF,mapping = aes(x = X, y = accuracy)) + geom_point() + geom_line(aes(group = 1)) + ggtitle("Accuracy for all algorithms") + xlab("")
-ggplot(data = algoDF,mapping = aes(x = X, y = precision)) + geom_point() + geom_line(aes(group = 1)) + ggtitle("Precision for all algorithms") + xlab("")
+ggplot(data = algoDF,mapping = aes(x = X, y = accuracy)) + geom_point() + geom_line(aes(group = 1)) + geom_text(aes(y = accuracy, label = accuracy,vjust = 1.2,hjust = 0.5)) + ggtitle("Accuracy for all algorithms") + xlab("")
+ggplot(data = algoDF,mapping = aes(x = X, y = precision)) + geom_point() + geom_line(aes(group = 1)) + ggtitle("Precision for all algorithms") + xlab("") + geom_text(aes(y = precision, label = precision,hjust = 0.5, vjust = 1))
 ggplot(data = algoDF,mapping = aes(x = X, y = recall)) + geom_point() + geom_line(aes(group = 1)) + ggtitle("Recall for all algorithms") + xlab("")
 ggplot(data = algoDF,mapping = aes(x = X, y = specificity)) + geom_point() + geom_line(aes(group = 1)) + ggtitle("Specificity for all algorithms") + xlab("")
 ggplot(data = algoDF,mapping = aes(x = X, y = f1Score)) + geom_point() + geom_line(aes(group = 1)) + ggtitle("f1scores for all algorithms") + xlab("")
